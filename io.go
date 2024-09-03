a7b092a59305f2bff6bddac708946cdda262dd81
Kai O'Reilly <koreilly5297@gmail.com>
2024-09-02 21:57:09 -0700

    clean up io logic

--- io.go
+++ io.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
+	"fmt"
 	"path/filepath"
 
+	"cogentcore.org/core/base/iox/jsonx"
 	"cogentcore.org/core/core"
 )
 
@@ -12,71 +12,58 @@ import (
 func (gr *Graph) SaveLast() { //types:add
 	if gr.State.File != "" {
 		TheGraph.SaveJSON(gr.State.File)
+	} else {
+		HandleError(fmt.Errorf("Graph.SaveLast: no last file"))
 	}
 }
 
 // OpenJSON opens a graph from a JSON file
 func (gr *Graph) OpenJSON(filename core.Filename) error { //types:add
-	b, err := os.ReadFile(string(filename))
-	if HandleError(err) {
-		return err
-	}
-	err = json.Unmarshal(b, gr)
+	err := jsonx.Open(gr, string(filename))
 	if HandleError(err) {
 		return err
 	}
 	gr.State.File = filename
 	// gr.AutoGraphAndUpdate()
-	return err
+	return nil
 }
 
 // OpenAutoSave opens the last graphed graph, stays between sessions of the app
 func (gr *Graph) OpenAutoSave() error {
-	b, err := os.ReadFile(filepath.Join(core.TheApp.AppDataDir(), "autosave.json"))
-	if HandleError(err) {
-		return err
-	}
-	err = json.Unmarshal(b, gr)
+	err := jsonx.Open(gr, filepath.Join(core.TheApp.AppDataDir(), "autosave.json"))
 	if HandleError(err) {
 		return err
 	}
 	// gr.AutoGraphAndUpdate()
-	return err
+	return nil
 }
 
 // SaveJSON saves a graph to a JSON file
 func (gr *Graph) SaveJSON(filename core.Filename) error { //types:add
-	var b []byte
 	var err error
 	if TheSettings.PrettyJSON {
-		b, err = json.MarshalIndent(gr, "", "  ")
+		err = jsonx.SaveIndent(gr, string(filename))
 	} else {
-		b, err = json.Marshal(gr)
+		err = jsonx.Save(gr, string(filename))
 	}
-	if HandleError(err) {
-		return err
-	}
-	err = os.WriteFile(string(filename), b, os.ModePerm)
 	if HandleError(err) {
 		return err
 	}
 	gr.State.File = filename
-	return err
+	return nil
 }
 
 // AutoSave saves the graph to autosave.json, called automatically
 func (gr *Graph) AutoSave() error {
-	var b []byte
+	filename := filepath.Join(core.TheApp.AppDataDir(), "autosave.json")
 	var err error
 	if TheSettings.PrettyJSON {
-		b, err = json.MarshalIndent(gr, "", "  ")
+		err = jsonx.SaveIndent(gr, filename)
 	} else {
-		b, err = json.Marshal(gr)
+		err = jsonx.Save(gr, filename)
 	}
 	if HandleError(err) {
 		return err
 	}
-	err = os.WriteFile(filepath.Join(core.TheApp.AppDataDir(), "autosave.json"), b, 0666)
-	HandleError(err)
-	return err
+	return nil
 }
